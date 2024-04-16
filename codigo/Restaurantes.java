811348f14b0a523aecec45cf49bf2622f783361f
Guilherme-RodriguesPuc <151187922+Guilherme-RodriguesPuc@users.noreply.github.com>
2024-04-16 17:18:57 -0300

    Codigo da Fila de Espera e Restaurantes
    O codigo FilaDeEspera foi feito por mim  e o de Restaurantes foi feito pelo Joao Tolentino

---
+++ codigo/Restaurantes.java
@@ -0,0 +1,11 @@
+public class Restaurantes{
+  //atributos
+  private Mesas quantidadeMesas[];
+
+  public Mesas getQuantidadeMesas(){
+    return quantidadeMesas;
+  }
+  public void setQuantidadeMesas(Mesas quantidadeMesas){
+    this.quantidadeMesas = quantidadeMesas;
+  }
+}
