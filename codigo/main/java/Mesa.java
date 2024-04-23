4b325b6f512b35f56d47b18351d6dd70803661b3
Guilherme-RodriguesPuc <151187922+Guilherme-RodriguesPuc@users.noreply.github.com>
2024-04-23 14:36:34 -0300

    Update Mesa.java

--- codigo/main/java/Mesa.java
+++ codigo/main/java/Mesa.java
@@ -1,26 +1,36 @@
-public class Mesa {
+class Mesa {
+
     private int numero;
-    private int capacidade;
     private boolean ocupada;
-    public Mesa(int capacidade) {
+    private int capacidade;
+
+    public Mesa(int numero, int capacidade) {
+        this.numero = numero;
+        this.ocupada = false;
         this.capacidade = capacidade;
     }
+
     public int getNumero() {
         return numero;
     }
+
     public void setNumero(int numero) {
         this.numero = numero;
     }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+
     public int getCapacidade() {
         return capacidade;
     }
+
     public void setCapacidade(int capacidade) {
         this.capacidade = capacidade;
-    }
-    public boolean estaDisponivel() {
-        return !ocupada;
-    }
-    public void setOcupada(boolean ocupada) {
-        this.ocupada = ocupada;
     }
 }
